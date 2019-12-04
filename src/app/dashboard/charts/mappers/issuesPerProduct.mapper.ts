0c696276dd708414b33b4e789f97074e50b110b9
AnatA <anat.alroy@cellebrite.com>
2019-12-04 18:07:19 +0200

    fix pareto chart

--- src/app/dashboard/charts/mappers/issuesPerProduct.mapper.ts
+++ src/app/dashboard/charts/mappers/issuesPerProduct.mapper.ts
@@ -1,18 +1,17 @@
-import { issuesPerProduct } from 'assets/mocks/issuesPerProduct';
 import { baseChart } from '../base-chart';
 
 export function issuesPerProductMapper(issuesPerProduct) {
-    const boxPlotDataSource = baseChart;
-    boxPlotDataSource.chart.caption = 'Issues Per Product';
-    boxPlotDataSource.chart.xaxisname = 'Product';
-    boxPlotDataSource.chart.yaxisname = 'Number of occurensess';
-    boxPlotDataSource.chart.theme = 'fusion';
-    boxPlotDataSource.chart.plottooltext = '$label reported <b>$datavalue</b> issues';
-    boxPlotDataSource.data = issuesPerProduct.issuesPerProduct.map(issue => {
+    const paretoDataSource = baseChart;
+    paretoDataSource.chart.caption = 'Issues Per Product';
+    paretoDataSource.chart.xaxisname = 'Product';
+    paretoDataSource.chart.yaxisname = 'Number of occurensess';
+    paretoDataSource.chart.theme = 'fusion';
+    paretoDataSource.chart.plottooltext = '$label reported <b>$datavalue</b> issues';
+    paretoDataSource.data = issuesPerProduct.issuesPerProduct.map(issue => {
         return {
             label: issue.product,
             value: issue.sumOfIssues
         }});
     
-        return boxPlotDataSource;
+        return paretoDataSource;
 }
