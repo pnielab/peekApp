c762572caad0b10072d17ae8c7a69479ea434106
Marom Gilboa <Marom.Gilboa@cellebrite.com>
2019-12-05 04:11:16 +0200

    fix charts

--- src/app/dashboard/charts/mappers/issues-per-product.mapper.ts
+++ src/app/dashboard/charts/mappers/issues-per-product.mapper.ts
@@ -1,21 +1,27 @@
 export function issuesPerProductMapper(issuesPerProduct) {
+  let totalSumOfIssues = 0;
+  issuesPerProduct.issuesPerProduct.forEach(issues => {
+    totalSumOfIssues += +issues.sumOfIssues;
+  });
   return {
     chart: {
-      xaxisname: 'Product',
-      yaxisname: 'Number of occurensess',
-      theme: 'fusion',
-      plottooltext: '$label reported <b>$datavalue</b> issues',
+      xaxisname: "Product",
+      yaxisname: "Number of occurensess",
+      theme: "fusion",
+      plottooltext: "$label reported <b>$datavalue</b> issues",
       plotSpacePercent: 0,
       stack100Percent: 1,
       decimals: 1,
       showPercentInTooltip: 0,
       showValues: 1,
       showPercentValues: 0,
+      numberSuffix: "%"
     },
     data: issuesPerProduct.issuesPerProduct.map(issue => {
       return {
         label: issue.product,
-        value: issue.sumOfIssues
+        value: (issue.sumOfIssues / totalSumOfIssues) * 100,
+        color: "#28a745"
       };
     })
   };
