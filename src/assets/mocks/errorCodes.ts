fcb58dfbfa73dd60c66f28a5b7ab8d482fdf68da
Marom Gilboa <Marom.Gilboa@cellebrite.com>
2019-12-05 02:29:55 +0200

    fix build

--- src/assets/mocks/errorCodes.ts
+++ src/assets/mocks/errorCodes.ts
@@ -1,85 +1,80 @@
 export const errorCodesData = {
-    'errorCodes': [
+  errorCodes: [
+    {
+      product: "PA",
+      featureName: "GPS",
+      errors: [
         {
-            'product': 'PA',
-            'featureName': 'GPS',
-            'errors': [
-                {
-                    'errorName': 'e1',
-                    'metaData':
-                    [
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trace this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trace this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trace this stack!'
-                        }
-                    ]
-                },
-                {
-                    'error': 'e2',
-                    'metaData':
-                    [
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        }
-                    ]
-                },
-                {
-                    'error':'e3',
-                    'metaData':
-                    [
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        },
-                        {
-                            'date': 'dd-MM-YYYY',
-                            'stackTrace': 'lets trcae this stack!'
-                        }
-                    ]
-                }
-            ]
+          errorName: "e1",
+          metaData: [
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trace this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trace this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trace this stack!"
+            }
+          ]
         },
         {
-            'product': 'UFED_4PC',
-            'featureName': '',
-            'errors': [
-                {
-                    'error':'e1',
-                    'date': 'dd-MM-YYYY',
-                    'sum':15
-                },
-                {
-                    'error':'e2',
-                    'date': 'dd-MM-YYYY',
-                    'sum': 23
-                },
-                {
-                    'error':'e3',
-                    'date': 'dd-MM-YYYY',
-                    'sum': 266
-                }
-            ]
+          error: "e2",
+          metaData: [
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            }
+          ]
+        },
+        {
+          error: "e3",
+          metaData: [
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            },
+            {
+              date: "dd-MM-YYYY",
+              stackTrace: "lets trcae this stack!"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      product: "UFED_4PC",
+      featureName: "",
+      errors: [
+        {
+          error: "e1",
+          date: "dd-MM-YYYY",
+          sum: 15
+        },
+        {
+          error: "e2",
+          date: "dd-MM-YYYY",
+          sum: 23
+        },
+        {
+          error: "e3",
+          date: "dd-MM-YYYY",
+          sum: 266
         }
       ]
     }
